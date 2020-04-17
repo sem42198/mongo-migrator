93d94a08fb87f72ed279faca92f47251136fa24b
Sarah Morrissey <s.morrissey@gatech.edu>
2020-04-16 20:08:46 -0400

    add date codec so date fields can be migrated to mongodb

--- codec_options.py
+++ codec_options.py
@@ -3,7 +3,7 @@ from bson.codec_options import TypeCodec
 from bson.codec_options import TypeRegistry
 from bson.codec_options import CodecOptions
 from decimal import Decimal
-
+import datetime
 class DecimalCodec(TypeCodec):
     python_type = Decimal    # the Python type acted upon by this type codec
     bson_type = Decimal128   # the BSON type acted upon by this type codec
@@ -15,8 +15,18 @@ class DecimalCodec(TypeCodec):
         """Function that transforms a vanilla BSON type value into our
         custom type."""
         return value.to_decimal()
+
+class DateCodec(TypeCodec):
+    python_type = datetime.date
+    bson_type = datetime.datetime
+    def transform_python(self, value):
+        return datetime.datetime.combine(value, datetime.time(0, 0, 0, 0))
+    def transform_bson(self, value):
+        return value.date()
+
 decimal_codec = DecimalCodec()
-type_registry = TypeRegistry([decimal_codec])
+date_codec = DateCodec()
+type_registry = TypeRegistry([decimal_codec, date_codec])
 codec_options = CodecOptions(type_registry=type_registry)
 
 def get():
