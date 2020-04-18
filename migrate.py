6033cbc1a7ccac1f26c02a95a2fdf94ee3277661
Sarah Morrissey <s.morrissey@gatech.edu>
2020-04-18 00:55:46 -0400

    get options working

--- migrate.py
+++ migrate.py
@@ -1,28 +1,20 @@
+import argparse
 import pymysql.cursors
 import pymongo
 from schema_graph import *
 
-def migrate():
-	mysql_host = 'localhost'
-	mysql_port = 3306
-	mysql_username = 'dev'
-	mysql_password = 'password'
+def migrate(args):
 
-	mongodb_host = 'localhost'
-	mongodb_port = 27017
-
-	db_name = 'classicmodels'
-
-	mysql_connection = pymysql.connect(host=mysql_host,
-	                             port=mysql_port,
-	                             user=mysql_username,
-	                             password=mysql_password,
-	                             db=db_name,
+	mysql_connection = pymysql.connect(host=args.mysql_host,
+	                             port=args.mysql_port,
+	                             user=args.mysql_username,
+	                             password=args.mysql_password,
+	                             db=args.database,
 	                             cursorclass=pymysql.cursors.DictCursor)
 
-	mongo_client = pymongo.MongoClient(mongodb_host, mongodb_port)
+	mongo_client = pymongo.MongoClient(args.mongodb_host, args.mongodb_port)
 
-	graph = Graph(mysql_connection, db_name)
+	graph = Graph(mysql_connection, args.database)
 	opts = graph.get_opts()
 
 	view_schemas(0, 5, opts)
@@ -51,4 +43,19 @@ def view_schemas(start, end, opts):
 		filename = input('Enter name of preview file: ')
 		schema.preview(filename, num_records)
 
-migrate()
+
+def main():
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--mysql-host', default='localhost', help='MySQL database host')
+	parser.add_argument('--mysql-port', default=3306, type=int, help='MySQL database port')
+	parser.add_argument('--mysql-username', required=True, help='MySQL username')
+	parser.add_argument('--mysql-password', required=True, help='MySQL password')
+	parser.add_argument('--mongodb-host', default='localhost', help='MongoDB host')
+	parser.add_argument('--mongodb-port', default=27017, type=int, help='MongoDB port')
+	parser.add_argument('database', help='Name of the MySQL database (MongoDB databse name will match)')
+	args = parser.parse_args()
+	migrate(args)
+
+
+if __name__ == '__main__':
+    main()
